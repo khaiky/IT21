db7491e5021fab35993a5c93d32298aa5645993a
hiether17 <lhietherfhiona@gmail.com>
2024-10-05 20:28:54 +0800

    Added object that calls its value through the name of the object

---
+++ objects-classes.js
@@ -0,0 +1,13 @@
+//Input
+const person = {
+    name: "Sammy",
+    age: 19,
+    gender: "Female"
+   }
+
+
+    //Process and Output
+    console.log("Hello There!");
+    console.log("Name: " + person.name);
+    console.log("Age: "  +person.age);
+    console.log("Gender: " + person.gender);
