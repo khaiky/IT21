2aecb9ef157dc0002492aee01d41ddc5c78ed55e
khaiky <samanthagabitano015@gmail.com>
2024-10-14 10:34:17 +0800

    added create line chart

--- chart.js
+++ chart.js
@@ -34,4 +34,61 @@ class LineChart extends ChartCreator {
         this.createLineChart(); 
     }
 }
+createLineChart() {
+    const chartData = {
+        labels: this.chartData.labels,
+        datasets: [{
+            label: 'Number of Students', 
+            data: this.chartData.data,
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 2,
+            tension: 0.4,  
+            pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+            pointRadius: 5,
+            pointHoverRadius: 7
+        }]
+    };
+    const config = {
+        type: 'line',
+        data: chartData,
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: '#e0e0e0'
+                    },
+                    ticks: {
+                        color: '#495057'
+                    }
+                },
+                x: {
+                    grid: {
+                        color: '#e0e0e0'
+                    },
+                    ticks: {
+                        color: '#495057'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                        color: '#333'
+                    }
+                },
+                tooltip: {
+                    enabled: true
+                }
+            }
+        }
+    };
+
+    new Chart(this.lineCtx, config); 
+}
 
