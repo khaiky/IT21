ab1e1e7631feb6edae3d58b6d433cc5d7aa7ad07
khaiky <samanthagabitano015@gmail.com>
2024-10-14 10:25:14 +0800

    added async method

--- chart.js
+++ chart.js
@@ -3,4 +3,10 @@ class ChartCreator {
         this.dataUrl = dataUrl;
         this.chartData = null;
     }
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
 }
