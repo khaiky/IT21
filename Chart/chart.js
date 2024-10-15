17372e3c28caeebef27d1f8747762a500d61f985
hiether17 <lhietherfhiona@gmail.com>
2024-10-15 21:00:52 +0800

    added some changes

--- Chart/chart.js
+++ Chart/chart.js
@@ -16,7 +16,6 @@ class ChartCreator {
                 throw new Error('Network response was not ok ' + response.statusText);
             }
 }
-    
 this.chartData = await response.json();
         } catch (error) {
             console.error('There has been a problem with your fetch operation:', error);
